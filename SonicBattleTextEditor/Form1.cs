2bc493d60be07fee56971f882df61c92f7450016
sahlaysta <sahlaysta>
2021-02-01 21:39:56 -0500

    wip

--- SonicBattleTextEditor/Form1.cs
+++ SonicBattleTextEditor/Form1.cs
@@ -14,32 +14,22 @@ namespace SonicBattleTextEditor
 {
     public partial class Form1 : Form
     {
-        private string dir = @Application.StartupPath;
-        private string sysLang = CultureInfo.InstalledUICulture.TwoLetterISOLanguageName;
+        private string Form2n = "Form2"; //"Select a language" text
         public Form1()
         {
             InitializeComponent();
-            setlang(sysLang);
-        }
-
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            
-        }
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            
+            setlang(Globals.sysLang);
         }
         private void setlang(string ls)
         {
-            string textdir = @Path.Combine(dir, "lang_" + ls + ".json");
+            string textdir = @Path.Combine(Globals.dir, ls + ".json");
             if (!(File.Exists(textdir)))
-                return;
+            {
+                if (@Directory.GetFiles(Path.Combine(Globals.dir), Globals.sysLang.Substring(0, Globals.sysLang.IndexOf('-') + 1) + "*.json").Length>0)
+                    textdir = @Directory.GetFiles(Path.Combine(Globals.dir), Globals.sysLang.Substring(0, Globals.sysLang.IndexOf('-') + 1) + "*.json")[0];
+                if (!(File.Exists(textdir)))
+                    return;
+            }
             string[] strings = System.IO.File.ReadAllText(textdir).Split(new string[] {System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             setmenustrings(strings);
         }
@@ -49,9 +39,34 @@ namespace SonicBattleTextEditor
             openToolStripMenuItem.Text = str[2];
             toolStripMenuItem1.Text = str[3];
             toolStripMenuItem3.Text = str[4];
+            this.Text = str[5];
+            Globals.lt1 = str[7];
+        }
+        private void languageselector()
+        {
+            Form2 form = new Form2();
+            form.Text = Form2n;
+            form.Size=new Size(400, 400);
+            form.ShowDialog();
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void toolStripMenuItem3_Click(object sender, EventArgs e) //change language
+        {
+            languageselector();
         }
 
-        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
---
+++ SonicBattleTextEditor/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SonicBattleTextEditor
+{
+    public partial class Form2 : Form
+    { 
+        public Form2()
+        {
+            InitializeComponent();
+            this.label1.Text = Globals.lt1 + " " + Globals.sysLang;
+            
+            List<string> jsonlist = new List<string>();
+            foreach (string str in Directory.GetFiles(Path.Combine(Globals.dir), "*.json")) {
+                jsonlist.Add(File.ReadLines(str).First());
+            }
+
+            listBox1.DataSource = jsonlist;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sel = listBox1.FindString(listBox1.SelectedItem.ToString());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
