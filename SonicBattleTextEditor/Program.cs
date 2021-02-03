306dd0c4a8c41b0af7160950d7a5d43fada1efaa
sahlaysta <sahlaysta>
2021-02-03 16:18:10 -0500

    localization support

--- SonicBattleTextEditor/Program.cs
+++ SonicBattleTextEditor/Program.cs
@@ -14,8 +14,24 @@ namespace SonicBattleTextEditor
         public static string dir = @Application.StartupPath;
         public static string sysLang = CultureInfo.InstalledUICulture.Name;
         public static string proLang = "";
+        public static string defaultLang = "en-US";
         public static string langExt = ".json";
         public static string[] strings = new string[] { "" };
+        public static string[] prefs = new string[] { "" };
+            public static string prefn = "sbte.prefs";
+        
+        public static bool prefsexists()
+        {
+            return File.Exists(Path.Combine(Globals.dir, prefn));
+        }
+        public static void readprefs()
+        {
+            prefs = System.IO.File.ReadAllText(Path.Combine(Globals.dir, prefn)).Split(new string[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        }
+        public static void saveprefs()
+        {
+            System.IO.File.WriteAllLines(Path.Combine(dir, prefn), prefs);
+        }
     }
     static class Program
     {
@@ -27,36 +43,58 @@ namespace SonicBattleTextEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            string[] defaultprefs = new string[] { "-1", "g" };
+            
+            if (Globals.prefsexists())
+            {
+                Globals.readprefs();
+            }
+            else
+            {
+                Globals.prefs = defaultprefs;
+                Globals.saveprefs();
+            }
 
             //Open system's language. If not found then open language select
             string langcode = Globals.sysLang;
+            if (Globals.prefs[0] != "-1")
+                langcode = Globals.prefs[0];
             bool lang = File.Exists(Path.Combine(Globals.dir, langcode + Globals.langExt));
-            if (!lang)
             {
-                if (langcode.Contains('-'))
-                    langcode = langcode.Substring(0, langcode.IndexOf('-'));
-                if (!langcode.Contains('-'))
+                if (!lang)
                 {
-                    if (Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt).Length > 0)
-                        langcode = Path.GetFileNameWithoutExtension(Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt)[0]);
-                    else
-                        langcode = "-1";
-                }
-                lang = Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt).Length > 0
-                       || File.Exists(Path.Combine(Globals.dir, langcode + Globals.langExt));
-                if (langcode == "-1")
-                {
-                    langcode = "en-US";
-                    lang = false;
+                    if (langcode.Contains('-'))
+                        langcode = langcode.Substring(0, langcode.IndexOf('-'));
+                    if (!langcode.Contains('-'))
+                    {
+                        if (Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt).Length > 0)
+                            langcode = Path.GetFileNameWithoutExtension(Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt)[0]);
+                        else
+                            langcode = "-1";
+                    }
+                    lang = Directory.GetFiles(Globals.dir, langcode + "-*" + Globals.langExt).Length > 0
+                           || File.Exists(Path.Combine(Globals.dir, langcode + Globals.langExt));
+                    if (langcode == "-1")
+                    {
+                        langcode = Globals.defaultLang;
+                        lang = false;
+                    }
                 }
             }
             Globals.proLang = langcode;
             Globals.strings = System.IO.File.ReadAllText(Path.Combine(Globals.dir, langcode + Globals.langExt)).Split(new string[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-
+            
             Form1 f1 = new Form1();
             Form2 f2 = new Form2();
             if (lang)
+            {
+                if (Globals.prefs[0] != langcode)
+                {
+                    Globals.prefs[0] = langcode;
+                    Globals.saveprefs();
+                }
                 Application.Run(f1);
+            }
             else
                 Application.Run(f2);
         }
