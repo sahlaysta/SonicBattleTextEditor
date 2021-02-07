aaf48f618a5ca5939e24dbf379dc03f671e52683
Todd Woodrow <tswoodrow@outlook.com>
2021-02-07 16:23:04 -0500

    textbox

--- SonicBattleTextEditor/Form1.cs
+++ SonicBattleTextEditor/Form1.cs
@@ -16,6 +16,8 @@ namespace SonicBattleTextEditor
     {
         private string rompath = "";
         private ValueTuple<string[], string[]> lib = (new string[0], new string[0]);
+        private BindingList<string> sbstrings = new BindingList<string>();
+        private int previndex = -2;
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +28,10 @@ namespace SonicBattleTextEditor
             toolStripMenuItem1.Text = Globals.strings[3];
             toolStripMenuItem3.Text = Globals.strings[4];
             tabPage1.Text = Globals.strings[12];
-            tabPage2.Text = Globals.strings[13];
+            textBox1.Enabled = false;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            sToolStripMenuItem.Text = Globals.strings[18];
+            sToolStripMenuItem.Enabled = false;
         }
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -39,13 +44,17 @@ namespace SonicBattleTextEditor
                 openFileDialog.Filter = Globals.strings[15] + " (*.*)|*.*|" + Globals.strings[16] + " (*.gba)|*.gba";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
+                openFileDialog.Title = Globals.strings[2];
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     rompath = openFileDialog.FileName;
                     listBox1.DataSource = new string[] { Globals.strings[17] };
-                    Globals.prefs[1] = rompath;
-                    Globals.saveprefs();
+                    if (Globals.prefs[1] != rompath)
+                    {
+                        Globals.prefs[1] = rompath;
+                        Globals.saveprefs();
+                    }
                 }
                 else
                 {
@@ -55,29 +64,43 @@ namespace SonicBattleTextEditor
 
             //read sb.lib file to tuple arrays
             string[] liblines = File.ReadAllLines(Path.Combine(Globals.dir, Globals.libn));
-            string[] liblinesx = new string[liblines.Length];
-            string[] liblinesy = new string[liblines.Length];
+            var temp = new List<string>();
+            foreach (var s in liblines)
+            {
+                if (!string.IsNullOrEmpty(s))
+                    temp.Add(s);
+            }
+            liblines = temp.ToArray();
+            string[] liblinesx = new string[liblines.Length/2];
+            string[] liblinesy = new string[liblines.Length/2];
             for (int i = 0; i < liblinesx.Length; i++)
-                liblinesx[i] = liblines[i].Substring(1, liblines[i].Substring(1).IndexOf('"'));
-            for (int i = 0; i < liblinesy.Length; i++)//3rd occurence of '"'
-                liblinesy[i] = liblines[i].Substring(liblines[i].Substring(1).IndexOf('"', liblines[i].Substring(1).IndexOf('"') + 1)).Replace("\"", "").Substring(1);
+                liblinesx[i] = liblines[i*2];
+            for (int i = 0; i < liblinesy.Length; i++)
+                liblinesy[i] = liblines[i*2+1];
             lib = new ValueTuple<string[], string[]> (liblinesx, liblinesy);
 
             StringBuilder rom = new StringBuilder();
             foreach (char ch in File.ReadAllBytes(rompath))
                 rom.AppendFormat(Convert.ToInt32(ch).ToString("X2"));
-
-            List<string> sbstrings = new List<string>();
+            List<string> sbstringst = new List<string>();
             string endline = "FEFF";
-            var rule = new[] { ("EDFE8C", 2299) };
-            foreach(var v in rule)
-                sbstrings.AddRange(rom.ToString().Substring(2 * getloc(v.Item1), (2 * getloc((v.Item2 * 4) + int.Parse(v.Item1, System.Globalization.NumberStyles.HexNumber))) - (2 * getloc(v.Item1))).Split(new string[] { endline }, StringSplitOptions.RemoveEmptyEntries));
+            var textobj = new[] { ("EDFE8C", 2299) };
+            foreach(var v in textobj)
+                sbstringst.AddRange(rom.ToString().Substring(2 * getloc(v.Item1), (2 * getloc((v.Item2 * 4) + int.Parse(v.Item1, System.Globalization.NumberStyles.HexNumber))) - (2 * getloc(v.Item1))).Split(new string[] { endline }, StringSplitOptions.RemoveEmptyEntries));
 
             //d(sbstrings.Aggregate((i, j) => i + j).Length);
-            for (int i = 0; i < sbstrings.Count; i++)
-                sbstrings[i] = readstring(sbstrings[i]);
+            for (int i = 0; i < sbstringst.Count; i++)
+                sbstringst[i] = readstring(sbstringst[i]);
 
+            foreach(string str in sbstringst)
+            {
+                sbstrings.Add(str);
+            }
+            sToolStripMenuItem.Enabled = true;
             listBox1.DataSource = sbstrings;
+            textBox1.Enabled = true;
+            listBox1.SelectedIndex = 1;
+            listBox1.SelectedIndex = 0;
         }
         private void d(object v)
         {
@@ -100,7 +123,7 @@ namespace SonicBattleTextEditor
             StringBuilder result = new StringBuilder();
             int q = 0;
             bool found = false;
-            for (int i = 0; i < v.Length; i+=4)
+            for (int i = 0; i < v.Length; i += 4)
             {
                 found = false;
                 q = 0;
@@ -122,7 +145,7 @@ namespace SonicBattleTextEditor
                             result.Append("<WHITE>");
                         else
                         {
-                            MessageBox.Show(Globals.strings[14] + ": FBFF" + v.Substring(i+4, 4));
+                            MessageBox.Show(Globals.strings[14] + ": FBFF" + v.Substring(i + 4, 4));
                             result.Append("□□");
                         }
 
@@ -148,8 +171,21 @@ namespace SonicBattleTextEditor
                     result.Append("□");
                 }
             }
-          
             return result.ToString();
+        }
+        private string match(string hex)
+        {
+            int i = 0;
+            foreach (string str in lib.Item2)
+            {
+                if (str == hex)
+                {
+                    return lib.Item1[i];
+                }
+                i++;
+            }
+
+            return "□";
         }
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
@@ -171,14 +207,55 @@ namespace SonicBattleTextEditor
         {
 
         }
+        private string replacen(string v)
+        {
+            if (v.Contains("\\n"))
+                return v.Replace("\\n", "\n");
+
+            return v;
+        }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (listBox1.SelectedIndex != -1)
+            {
+                if (previndex != listBox1.SelectedIndex)
+                {
+                    textBox1.Enabled = false;
+                    textBox1.Clear();
+                    foreach (char ch in replacen(listBox1.Items[listBox1.SelectedIndex].ToString()))
+                    {
+                        if (ch == '\n')
+                        {
+                            textBox1.AppendText(Environment.NewLine);
+                        }
+                        else
+                            textBox1.AppendText(ch.ToString());
+                    }
+                }
+                textBox1.Enabled = true;
+                previndex = listBox1.SelectedIndex;
+            }
         }
         private void listBox1_DoubleClick(object sender, MouseEventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled)
+            {
+                listBox1.BeginUpdate();
+                sbstrings[listBox1.SelectedIndex] = textBox1.Text.Replace("\n", "\\n");
+                listBox1.EndUpdate();
+                this.ActiveControl = textBox1;
+            }
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            d(1);
         }
     }
 }
